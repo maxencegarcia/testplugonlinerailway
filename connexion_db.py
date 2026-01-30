53e4ab53bf56b2645efe2d212f6badc9fb2feda3
mgarcia6 <saeinfob1@gmail.com>
2026-01-30 15:39:55 +0100

    Update connexion_db.py

--- connexion_db.py
+++ connexion_db.py
@@ -18,28 +18,3 @@ def get_db():
         cursorclass=pymysql.cursors.DictCursor
     )
     return db
-    
-def activate_db_options(db):
-    cursor = db.cursor()
-    # Vérifier et activer l'option ONLY_FULL_GROUP_BY si nécessaire
-    cursor.execute("SHOW VARIABLES LIKE 'sql_mode'")
-    result = cursor.fetchone()
-    if result:
-        modes = result['Value'].split(',')
-        if 'ONLY_FULL_GROUP_BY' not in modes:
-            print('MYSQL : il manque le mode ONLY_FULL_GROUP_BY')   # mettre en commentaire
-            cursor.execute("SET sql_mode=(SELECT CONCAT(@@sql_mode, ',ONLY_FULL_GROUP_BY'))")
-            db.commit()
-        else:
-            print('MYSQL : mode ONLY_FULL_GROUP_BY  ok')   # mettre en commentaire
-    # Vérifier et activer l'option lower_case_table_names si nécessaire
-    cursor.execute("SHOW VARIABLES LIKE 'lower_case_table_names'")
-    result = cursor.fetchone()
-    if result:
-        if result['Value'] != '0':
-            print('MYSQL : valeur de la variable globale lower_case_table_names differente de 0')   # mettre en commentaire
-            cursor.execute("SET GLOBAL lower_case_table_names = 0")
-            db.commit()
-        else :
-            print('MYSQL : variable globale lower_case_table_names=0  ok')    # mettre en commentaire
-    cursor.close()
